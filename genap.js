9a6609cf9275232cdf8ffe301a8c745e126c8dcb
monnttt <118706500+monnttt@users.noreply.github.com>
2022-11-21 22:54:12 -0800

    first commit

---
+++ genap.js
@@ -0,0 +1,4 @@
+var e = 2;
+while (e <= 10){
+    if (e%2==0)
+    console.log(e); e++}
---
+++ i.js
@@ -0,0 +1,11 @@
+let y = 2;
+
+for (let y = 0; y <= 10; y++)
+if(y%2==0)
+{console.log(y)}
+
+/*let y = 2;
+
+for (let y = 0; y <= 10; y++)
+if(y&2==0)
+{console.log(y)}*/
